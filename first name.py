894b3f06025ae6d9b407b1614087fbf3407accc9
Leon Oram <45027@longroad.ac.uk>
2014-09-09 12:53:24 +0100

    Hello World/First Name

---
+++ first name.py
@@ -0,0 +1,12 @@
+#Leon Oram
+#09-09-2014
+#Excersise 1.1 - Hello world
+
+#Prints the line in the quotation marks
+print("Hello World!")
+#Allows the user to type in their name and saves it as the variable 'first_name'
+first_name = input("Please enter your name: ")
+#Prints the variable 'first_name' which contains the users inputed name
+print(first_name)
+#Prints 'Hi' then the vaiable 'first_name'
+print("Hi {0}!".format(first_name))
