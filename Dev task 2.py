c94878439b2444efcfc801ec287f18822b0dae35
Leon Oram <45027@longroad.ac.uk>
2014-09-12 14:12:23 +0100

    Task 2 compl

---
+++ Dev task 2.py
@@ -0,0 +1,7 @@
+#Leon Oram
+#12-09-2014
+#Development task 2
+
+temp_fahren = int(input("Please enter the tempreature in fahrenheit:"))
+temp_cent = (temp_fahren -32)*(5/9)
+print("The tempreature in centigrade is {0}".format(temp_cent))
