7024b43963033b605fa869ff0e7d045b97e5c0b9
Leon Oram <45027@longroad.ac.uk>
2014-09-12 13:45:29 +0100

    Examples

---
+++ Total Score.py
@@ -0,0 +1,8 @@
+#Leon Oram
+#12-09-2014
+#Test Scores
+
+first_score = int(input("Please enter the first test score: "))
+second_score = int(input("Please enter the second test score: "))
+total_score = first_score + second_score
+print("The total score is {0}".format(total_score))
---
+++ assignment_improvement_exercise edit.py
@@ -0,0 +1,16 @@
+#john bain
+#variable improvement exercise
+#05-09-12
+
+import math
+
+radius = int(input("please enter the radius of the circle: "))
+
+circumf = 2* math.pi * radius
+circumf = round(circumf,2)
+
+area = math.pi * radius**2
+area = round(area,2)
+
+print("The circumference of this circle is {0}.".format(circumf))
+print("The area of this circle is {0}.".format(area))
