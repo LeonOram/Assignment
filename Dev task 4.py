b868a7a3d55f6f63d1f787653a6ef89642b3707a
Leon Oram <45027@longroad.ac.uk>
2014-09-14 21:32:06 +0100

    Dev task 4 completed

--- Dev task 4.py
+++ Dev task 4.py
@@ -2,7 +2,11 @@
 #12-09-2014
 #Dev task 4
 
-pool_length
-pool_width
-pool_deepest
-pool_shallowest
+pool_length = float(input("Please enter the length of the pool in meters:"))
+pool_width = float(input("Please enter the width of the pool in meters:"))
+pool_deepest = float(input("Please enter the depth of the deepest point of the pool in meters:"))
+pool_shallowest = float(input("Please enter the depth of the shallowest point of the pool in meters:"))
+pool_ave_depth = (pool_deepest + pool_shallowest) / 2
+pool_volume_cmet = pool_length * pool_width * pool_ave_depth
+pool_volume_litres = pool_volume_cmet * 1000
+print("The total volume of the pool is {0} cubic meters or {1} litres".format(pool_volume_cmet,pool_volume_litres))
