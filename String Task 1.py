373bc93b29fdb7996c04b1eda00886e638781c0b
Leon Oram <45027@longroad.ac.uk>
2014-10-06 21:04:10 +0100

    comp

---
+++ String Task 1.py
@@ -0,0 +1,12 @@
+#Leon Oram
+#06-10-2014
+#String Task 1
+
+string = input("Please enter your quote: ")
+
+upper = string.upper()
+lower = string.lower()
+caps = string.capitalize()
+title = string.title()
+
+print("{0}: {1}: {2}: {3}".format(upper,lower,caps,title))
